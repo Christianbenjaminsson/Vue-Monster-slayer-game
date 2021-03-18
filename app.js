2a5af4b9b45ea09f4c53af1db06bec218be67019
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-03-18 11:36:03 +0100

    Create and display log messages on all game actions

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessage: [],
     };
   },
   watch: {
@@ -49,16 +50,19 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "special-attack", attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -69,6 +73,7 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage("player", "heal", healValue);
       this.attackPlayer();
     },
     startNewGame() {
@@ -76,10 +81,19 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessage = [];
     },
     surrender() {
       this.winner = "monster";
       this.playerHealth = 0;
+      this.addLogMessage("player", "surrender", null);
+    },
+    addLogMessage(who, what, value) {
+      this.logMessage.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
